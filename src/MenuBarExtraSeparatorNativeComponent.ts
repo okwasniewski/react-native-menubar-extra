8b68eb959cce0e0987a692e9e3ad84ed2d56b84d
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2023-09-18 19:12:43 +0200

    feat: add documentation in JSDoc

--- src/MenuBarExtraSeparatorNativeComponent.ts
+++ src/MenuBarExtraSeparatorNativeComponent.ts
@@ -1,5 +1,9 @@
-import type { ViewProps } from 'react-native';
+import { Platform, type ViewProps } from 'react-native';
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
+
+if (Platform.OS !== 'macos') {
+  throw new Error('react-native-menubar-extra is only supported on macOS.');
+}
 
 interface NativeProps extends ViewProps {}
 
